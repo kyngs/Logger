89deab2e62459e68377f2a74eeabc67563d1d2e5
kyngs <snezbeda@gmail.com>
2021-03-09 13:52:23 +0100

    Hotfix

--- Logger/src/main/java/xyz/kyngs/logger/Logger.java
+++ Logger/src/main/java/xyz/kyngs/logger/Logger.java
@@ -10,19 +10,19 @@ public interface Logger {
     PrintStream OUT = System.out, ERR = System.err;
 
     default void info(Object object, Throwable... throwables) {
-        log(object, INFO, 3, Thread.currentThread(), LocalTime.now(), throwables);
+        log(object, INFO, 2, Thread.currentThread(), LocalTime.now(), throwables);
     }
 
     default void warn(Object object, Throwable... throwables) {
-        log(object, WARN, 3, Thread.currentThread(), LocalTime.now(), throwables);
+        log(object, WARN, 2, Thread.currentThread(), LocalTime.now(), throwables);
     }
 
     default void error(Object object, Throwable... throwables) {
-        log(object, ERROR, 3, Thread.currentThread(), LocalTime.now(), throwables);
+        log(object, ERROR, 2, Thread.currentThread(), LocalTime.now(), throwables);
     }
 
     default void debug(Object object, Throwable... throwables) {
-        log(object, DEBUG, 3, Thread.currentThread(), LocalTime.now(), throwables);
+        log(object, DEBUG, 2, Thread.currentThread(), LocalTime.now(), throwables);
     }
 
     default void log(Object object, LogLevel debug, int i, Thread currentThread, LocalTime now, Throwable... throwables) {
--- Logger/src/main/java/xyz/kyngs/logger/Test.java
+++ Logger/src/main/java/xyz/kyngs/logger/Test.java
@@ -3,13 +3,6 @@ package xyz.kyngs.logger;
 public class Test {
 
     public static void main(String[] args) throws InterruptedException {
-        //Logger logger = LogBuilder
-        //        .async()
-        //        .build();
-        //logTest(logger);
-        //Thread.sleep(1000);
-        ////logger.destroy();
-//        Thread.currentThread().setUncaughtExceptionHandler((t, e) -> Logger.ERR.print(e.getMessage()));
         Logger logger = LogBuilder
                 .async()
                 .build();
